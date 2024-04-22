efdf2036d21ac5ec46379ce203218c4f0b2a9b93
tource <tource20@naver.com>
2024-04-22 17:56:33 +0900

    featur: 목록 json 연동 처리

--- public/www/js/crew.js
+++ public/www/js/crew.js
@@ -1,5 +1,44 @@
 // html 을 제어하므로
 window.addEventListener("load", function () {
+  // html 에 내용 출력하기
+  const makeHtml = (_result) => {
+    //   console.log(_result);
+    // 할일작성
+    // 전체 html 저장용 일반 변수
+    let allTag = "";
+    const crew = document.querySelector("#crew");
+    // 아래 코드는 최적화 실패
+    // const total = _result.length;
+    // for (let i = 0; i < total; i++) {
+    //   const obj = _result[i];
+    //   const tag = `<a href=${obj.link} class="list-box">
+    //       <div class="list-box-img br-20" style="background: url('./images/${obj.imgpath}') no-repeat center; background-size: cover"></div>
+    //       <div class="list-box-cate">
+    //         <img src="./images/icon/${obj.icon}" alt="${obj.category}" />
+    //         <span style="color:${obj.txtcolor};">${obj.category}</span>
+    //       </div>
+    //       <p class="list-box-title">${obj.title}</p>
+    //       <span class="list-box-day">${obj.day}</span>
+    //       </a>`;
+    //   allTag = allTag + tag;
+    // }
+    // 추천합니다. (조건은 대상이 배열인 경우)
+    _result.forEach((item) => {
+      const tag = `<a href=${item.link} class="list-box">
+          <div class="list-box-img br-20" style="background: url('./images/${item.imgpath}') no-repeat center; background-size: cover"></div>
+          <div class="list-box-cate">
+            <img src="./images/icon/${item.icon}" alt="${item.category}" />
+            <span style="color:${item.txtcolor};">${item.category}</span>
+          </div>
+          <p class="list-box-title">${item.title}</p>
+          <span class="list-box-day">${item.day}</span>
+          </a>`;
+      allTag = allTag + tag;
+    });
+
+    crew.innerHTML = allTag;
+  };
+
   //할일
   const dataUrl = "./apis/crew.json";
   fetch(dataUrl)
@@ -9,33 +48,7 @@ window.addEventListener("load", function () {
       return result;
     })
     .then((result) => {
-      //   console.log(result);
-      // 할일작성
-      // 전체 html 저장용 일반 변수
-      let allTag = "";
-      const crew = document.querySelector("#crew");
-      for (let i = 0; i < result.length; i++) {
-        const obj = result[i];
-        const tag = `<a href=${obj.link} class="list-box">
-          <div
-            class="list-box-img br-20"
-            style="
-              background: url('./images/${obj.imgpath}') no-repeat center;
-              background-size: cover;
-            "
-          ></div>
-          <div class="list-box-cate">
-            <img src="./images/icon/${obj.icon}" alt="${obj.category}" />
-            <span style="color:${obj.txtcolor};">${obj.category}</span>
-          </div>
-          <p class="list-box-title">
-          ${obj.title}
-          </p>
-          <span class="list-box-day">${obj.day}</span>
-        </a>`;
-        allTag = allTag + tag;
-      }
-      crew.innerHTML = allTag;
+      makeHtml(result);
     })
     .catch((error) => {
       console.log(error);
--- public/www/js/news.js
+++ public/www/js/news.js
@@ -14,27 +14,51 @@ window.addEventListener("load", function () {
       // 전체 html 저장용 일반 변수
       let allTag = "";
       const news = document.querySelector("#news");
-      for (let i = 0; i < result.length; i++) {
-        const obj = result[i];
-        const tag = `<a href=${obj.link} class="list-box">
+
+      // for (let i = 0; i < result.length; i++) {
+      //   const obj = result[i];
+      //   const tag = `<a href=${obj.link} class="list-box">
+      //   <div
+      //     class="list-box-img br-20"
+      //     style="
+      //       background: url('./images/${obj.imgpath}') no-repeat center;
+      //       background-size: cover;
+      //     "
+      //   ></div>
+      //   <div class="list-box-cate">
+      //     <img src="./images/icon/${obj.icon}" alt="${obj.category}" />
+      //     <span style="color:${obj.txtcolor};">${obj.category}</span>
+      //   </div>
+      //   <p class="list-box-title">
+      //   ${obj.title}
+      //   </p>
+      //   <span class="list-box-day">${obj.day}</span>
+      // </a>`;
+      //   allTag = allTag + tag;
+      // }
+
+      // 배열이라면 반복하자.
+      result.forEach((item) => {
+        const tag = `<a href=${item.link} class="list-box">
         <div
           class="list-box-img br-20"
           style="
-            background: url('./images/${obj.imgpath}') no-repeat center;
+            background: url('./images/${item.imgpath}') no-repeat center;
             background-size: cover;
           "
         ></div>
         <div class="list-box-cate">
-          <img src="./images/icon/${obj.icon}" alt="${obj.category}" />
-          <span style="color:${obj.txtcolor};">${obj.category}</span>
+          <img src="./images/icon/${item.icon}" alt="${item.category}" />
+          <span style="color:${item.txtcolor};">${item.category}</span>
         </div>
         <p class="list-box-title">
-        ${obj.title}
+        ${item.title}
         </p>
-        <span class="list-box-day">${obj.day}</span>
+        <span class="list-box-day">${item.day}</span>
       </a>`;
         allTag = allTag + tag;
-      }
+      });
+
       news.innerHTML = allTag;
     })
     .catch((error) => {
